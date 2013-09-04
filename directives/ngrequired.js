e02f8ed6ef3804815884614dd875080a08f084dd
Michael Robinson <mike@pagesofinterest.net>
2013-09-05 02:20:21 +1200

    Comment out timing code pending rewrite.

--- directives/ngrequired.js
+++ directives/ngrequired.js
@@ -8,31 +8,31 @@ angular.module('required', [])
         }
         element.after('<div class="required-tick">*</div>');
 
-        var $element = $(element[0]),
-          $tick = $element.next();
+        //var $element = $(element[0]),
+        //  $tick = $element.next();
 
-        function checkInput(event, callerElement) {
-          if ($element.val() === '') {
-              $tick.show();
-          } else {
-            // Trigger NG's validity check
-            $element.trigger('input');
-            if ($element.hasClass('ng-invalid')) {
-              $tick.show();
-            } else {
-              $tick.hide();
-            }
-          }
-          if ($element !== callerElement) {
-            $timeout(function() {
-              scope.$broadcast('checkInput', $element);
-            }, 2500);
-          }
-        }
-        scope.$on('checkInput', checkInput);
-        $timeout(function() {
-          scope.$broadcast('checkInput', $element);
-        }, 0);
+        //function checkInput(event, callerElement) {
+        //  if ($element.val() === '') {
+        //      $tick.show();
+        //  } else {
+        //    // Trigger NG's validity check
+        //    $element.trigger('input');
+        //    if ($element.hasClass('ng-invalid')) {
+        //      $tick.show();
+        //    } else {
+        //      $tick.hide();
+        //    }
+        //  }
+        //  if ($element !== callerElement) {
+        //    $timeout(function() {
+        //      scope.$broadcast('checkInput', $element);
+        //    }, 2500);
+        //  }
+        //}
+        //scope.$on('checkInput', checkInput);
+        //$timeout(function() {
+        //  scope.$broadcast('checkInput', $element);
+        //}, 0);
       }
     }
   }
